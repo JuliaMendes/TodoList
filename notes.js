85cc528692312b0de7b811fab62236c9dc4819f1
julia.mendes <juliadepaivams@gmail.com>
2022-07-11 09:54:39 -0300

    first commit

---
+++ notes.js
@@ -0,0 +1,36 @@
+////Aula 06 ////
+// ParentNode
+//  const pai = paleta1.parentNode
+// console.log(pai)
+
+// //Children
+// const filhos = pai.children
+// console.log(filhos)
+
+// //FirstElementChild / LastElementChild
+// const primeiroElemento = pai.firstElementChild
+// const ultimoElemento = pai.lastElementChild
+// console.log(primeiroElemento)
+// console.log(ultimoElemento)
+
+// //PreviousElementSibling / NextElementSibling
+
+// const irmaoAntecessor = paleta2.previousElementSibling
+// const irmaoSucessor = paleta2.nextElementSibling
+// console.log(irmaoAntecessor)
+// console.log(irmaoSucessor)
+
+////Aula 07 ////
+
+//CreateElement, InsertBefore e AppendChild
+
+// const titulo = document.createElement("h1")
+// titulo.textContent = "To Do List"
+//  console.log(titulo)
+
+// const body = document.querySelector("body")
+// const paletas_div = document.querySelector(".paletas_div")
+// body.insertBefore(titulo, paletas_div)
+
+// const paleta4 = document.createElement("div")
+// paletas_div.appendChild(paleta4)
